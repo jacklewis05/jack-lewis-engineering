8835b5cca4be66aac633833fb8a361aa746aee89
jacklewis05 <jack@arachnerd.com>
2026-09-24 13:54:37 -0400

    changed repository

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite'
 // Repo name is "portfolio", deployed to https://jacklewis05.github.io/po
 // If you ever rename the GitHub repo, update this `base` to match: '/<repo-name>/'
 export default defineConfig({
-  base: '/jack-lewis-website/',
+  base: '/jack-lewis-engineering/',
   plugins: [react(), tailwindcss()],
 })
